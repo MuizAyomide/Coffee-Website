83577f6f966c4ed7681d92b217a9db8039a6bfb0
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-23 23:03:11 +0100

    commit

--- coffee-app/src/App.jsx
+++ coffee-app/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from './Component/Home/Home'
+import About from './Component/About/About'
+import Review from './Component/Review/Review'
 
 const App = () => {
   return (
     <div>
       <Home/>
+      <About/>
+      <Review/>
     </div>
   )
 }
