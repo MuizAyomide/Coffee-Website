c9cd9f1db705b163fffc6fb81a2d28076fa34bff
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-23 23:03:52 +0100

    commit

--- coffee-app/src/Component/Home/Home.jsx
+++ coffee-app/src/Component/Home/Home.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div className='home'>
-        <div className="inner-content">
-            <div className="nav">
-                <div className="logo">
-                    <img src="logo.png" alt="" />
-                </div>
-                <div className="nav-sec">
-                    <ul>
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Features</li>
-                        <li>Purchase</li>
-                        <li>Contact Us</li>
-                    </ul>
-                </div>
-            </div>
+    <div className="home">
+      <div className="main-bg"></div>
+      <div className="contents">
+        <div className="nav-content">
+          <div className="logo">
+            <img src="logo.png" alt="" />
+          </div>
+          <div className="nav-menu">
+            <ul className="nav-ul">
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Features</li>
+              <li>Purchase</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
         </div>
 
-        <div className="text">
-            <h1>
-                Coffee Makes Mood
-            </h1>
-            <p>"Indulge in Every Sips Experience the Richness of Coffee!"</p>
+        <div className="text-content">
+          <h3>Coffee Makes Mood</h3>
+          <p>"Indulge in Every Sip: Experience the Richness of Coffee!"</p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
