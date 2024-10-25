813cfd163b18017909293724c3ec656f298cf523
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-25 13:21:14 +0100

    commit

--- coffee-app/src/Component/Home/Home.jsx
+++ coffee-app/src/Component/Home/Home.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 
 const Home = () => {
+
+const [open, setOpen] = useState(false);
+
   return (
     <div className="home">
       <div className="main-bg"></div>
@@ -10,8 +13,10 @@ const Home = () => {
           <div className="logo">
             <img src="logo.png" alt="" />
           </div>
-          <div className="nav-menu">
+          <div className="top nav-menu">
             <ul className="nav-ul">
+
+            
               <li>Home</li>
               <li>About Us</li>
               <li>Features</li>
@@ -19,6 +24,8 @@ const Home = () => {
               <li>Contact Us</li>
             </ul>
           </div>
+          
+
         </div>
 
         <div className="text-content">
@@ -26,7 +33,23 @@ const Home = () => {
           <p>"Indulge in Every Sip: Experience the Richness of Coffee!"</p>
         </div>
       </div>
+      {
+        open === true ?
+      <div className="nav-menu">
+        <ul className="nav-ul">
+      
+        <p onClick={()=>setOpen(false)} className="close ">X</p>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Features</li>
+          <li>Purchase</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      : <p onClick={()=>setOpen(true)} className="bar">=</p>
+      }
     </div>
+
   );
 };
 
