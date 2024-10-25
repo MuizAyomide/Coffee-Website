9d1b0ecb04aa844113b68061085737a4adf7996d
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-25 13:21:06 +0100

    commit

---
+++ coffee-app/src/Component/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright 2020 All Right Reserved Coffee Borcelle</p>
+    </div>
+  )
+}
+
+export default Footer
